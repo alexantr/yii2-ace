a4e5f5916f9391e6ce84164624ff521c9d4d9e78
Alex Yashkin <alex.yashkin@gmail.com>
2017-08-06 17:38:57 +0300

    Removed unused const

--- src/AceAsset.php
+++ src/AceAsset.php
@@ -6,8 +6,6 @@ use yii\web\AssetBundle;
 
 class AceAsset extends AssetBundle
 {
-    const CDN_BASE_PATH = 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.8';
-
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $js = [
